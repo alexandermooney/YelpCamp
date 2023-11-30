58177edd1d22aecbcc279752c5be3cb56c6c3ae7
alexandermooney <alexmooneydev@gmail.com>
2023-11-29 23:40:36 -0500

    Add bootstrap and basic styles

--- seeds/index.js
+++ seeds/index.js
@@ -17,9 +17,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const campPrice = Math.floor(Math.random() * 21) + 10;
         const camp = new Campground({
             title: `${sample(descriptors)} ${sample(places)}`,
-            location: `${cities[random1000].city}, ${cities[random1000].state}`
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elementum sagittis vitae et leo duis. Arcu risus quis varius quam. Erat imperdiet sed euismod nisi. Varius sit amet mattis vulputate enim nulla aliquet porttitor. Rhoncus dolor purus non enim praesent elementum facilisis leo vel.',
+            price: campPrice
         });
         await camp.save();
     }
